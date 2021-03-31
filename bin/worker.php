02fea68dd439423e24b372a34e7e8e2427c7a73b
you tou <d.yang@triplevantage.com>
2021-03-31 18:53:47 +0900

    added database access1

--- bin/worker.php
+++ bin/worker.php
@@ -1,7 +1,35 @@
 <?php
 
-echo 'hello world';
+$app = new Silex\Application();
+$app['debug'] = true;
 
-print("hello world!");
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
 
-?>
+//   $sql = "insert into test_table(id,name) values(2,'hello world')";
+//   $stmt = $app['pdo']->prepare($sql);
+//   $stmt->execute();
+
+  $st = $app['pdo']->prepare('SELECT LastName FROM salesforce001.Lead');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['LastName']);
+    $LastName[] = $row;
+    echo $LastName[]
+  }
+  
+$app->run();
+
