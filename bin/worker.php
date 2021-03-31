e701e5ad54fb3fa947389bd91cd9bdda1a221f9b
you tou <d.yang@triplevantage.com>
2021-03-31 20:05:46 +0900

    added4

--- bin/worker.php
+++ bin/worker.php
@@ -11,8 +11,11 @@ $DBPASS = $dbopts["pass"];
 try{
   //DB接続
   $dbh = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");
+  
+  //A社データ処理（中間テーブル取り込み）
    //SQL作成
-  $sql = 'select LastName,FirstName from salesforce001.Lead';
+  //$sql = 'select LastName,FirstName,Email from salesforce001.Lead';
+    $sql = 'select * from salesforce001.Lead';
   //SQL例
   //$sql = 'select * from "SchemeName"."TableName"';
 
@@ -20,12 +23,43 @@ try{
   foreach ($dbh->query($sql) as $row) {
       //指定Columnを一覧表示
 
-      print($row['firstname']);      
-      print($row['lastname']);
-      $name = $row['lastname'];
-      print($name);
-
+      print($row['firstname'].'\t');      
+      print($row['lastname'].'\t');
+      print($row['sfid'].'\t');      
+      print($row['schema'].'\t');
+      print($row['email']);
+     // $name = $row['lastname'];
+     $sfid=$row['sfid'];
+     $schema=$row['schema'];
+      print("======salesforce001.Lead=========");
+      //中間テーブル登録
+      $prepIns001 = $dbh->prepare('INSERT INTO sfdcmiddle.middle_lead(sfid, schema) VALUES($sfid, $schema)');
+      $prepIns001->execute();
   }
+
+  //B社データ処理（中間テーブル取り込み）
+  //SQL作成
+  //$sql = 'select LastName,FirstName,Email from salesforce002.Lead';
+  $sql = 'select * from salesforce002.Lead';
+    //SQL実行
+  foreach ($dbh->query($sql) as $row) {
+      //指定Columnを一覧表示
+
+      print($row['firstname'].'\t');      
+      print($row['lastname'].'\t');
+      print($row['sfid'].'\t');      
+      print($row['schema'].'\t');
+      print($row['email']);
+     // $name = $row['lastname'];
+     $sfid=$row['sfid'];
+     $schema=$row['schema'];
+      print("======salesforce002.Lead=========");
+      
+      //中間テーブル登録
+      $prepIns002 = $dbh->prepare('INSERT INTO sfdcmiddle.middle_lead(sfid, schema) VALUES($sfid, $schema)');
+      $prepIns002->execute();
+  }
+  
 
 }catch(PDOException $e){
   print("接続失敗");
