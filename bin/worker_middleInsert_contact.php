ecca3d33b112b82a63c58f26a6c862b5f20648d1
you tou <d.yang@triplevantage.com>
2021-04-07 17:45:53 +0900

    added7

--- bin/worker_middleInsert_contact.php
+++ bin/worker_middleInsert_contact.php
@@ -1,5 +1,9 @@
 <?php
 
+//定数ファイルを読み込み
+require('config.php');
+
+//DB接続情報を取得
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
 $DBHOST = $dbopts["host"];
@@ -12,61 +16,36 @@ try{
   //DB接続
   $dbh = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");
   
-  //A社データ処理（中間テーブル取り込み）
-   //SQL作成
-  //$sql = 'select LastName,FirstName,Email from salesforce001.Lead';
-    $sql = 'select * from salesforce001.Lead';
-  //SQL例
-  //$sql = 'select * from "SchemeName"."TableName"';
+     foreach ($schema as $value) {
+   
+       //検索対象スキーマ
+       $schemaid = $value.'.contact';
+       
+       //データ処理（中間テーブル取り込み）
+       //SQL作成
+       $sql = 'select * from '.$schemaid;
 
-  //SQL実行
-  foreach ($dbh->query($sql) as $row) {
-      //指定Columnを一覧表示
+      //SQL実行
+      foreach ($dbh->query($sql) as $row) {
+        //指定Columnを一覧表示
 
-      print($row['firstname'].' ');      
-      print($row['lastname'].' ');
-      print($row['sfid'].' ');
-      print($row['email']);
-     $id = $row['id'];
-     $sfid=$row['sfid'];
-     $schema='salesforce001';
-     $firstname = $row['firstname'];
-     $lastname = $row['lastname'];
-     $email = $row['email'];    
-      print("======salesforce001.Lead=========");
-      //中間テーブル登録
-      $prepIns001 = $dbh->prepare('INSERT INTO sfdcmiddle.middle_lead(id,sfid, schema,firstname,lastname,email) VALUES(:id,:sfid, :schema,:firstname,:lastname,:email)');
-      $prepIns001->execute(array($id,$sfid,$schema,$firstname,$lastname,$email));
+        print($row['firstname'].' ');      
+        print($row['lastname'].' ');
+        print($row['sfid'].' ');
+        print($row['email']);
+        $id = $row['id'];
+        $sfid=$row['sfid'];
+        $schema=$value;
+        $firstname = $row['firstname'];
+        $lastname = $row['lastname'];
+        $email = $row['email'];    
+        print('======salesforce.lead========='.$schemaid);
+        //中間テーブル登録
+        $prepIns001 = $dbh->prepare('INSERT INTO sfdcmiddle.middle_contact(id,sfid, schema,firstname,lastname,email) VALUES(:id,:sfid, :schema,:firstname,:lastname,:email)');
+        $prepIns001->execute(array($id,$sfid,$schema,$firstname,$lastname,$email));
       
-      
-      
+    }
   }
-
-  //B社データ処理（中間テーブル取り込み）
-  //SQL作成
-  //$sql = 'select LastName,FirstName,Email from salesforce002.Lead';
-  $sql = 'select * from salesforce002.Lead';
-    //SQL実行
-  foreach ($dbh->query($sql) as $row) {
-      //指定Columnを一覧表示
-
-      print($row['firstname'].' ');      
-      print($row['lastname'].' ');
-      print($row['sfid'].' ');      
-      print($row['email']);
-     $id=$row['id'];
-     $sfid=$row['sfid'];
-     $schema='salesforce002';
-     $firstname = $row['firstname'];
-     $lastname = $row['lastname'];
-     $email = $row['email'];    
-      print("======salesforce002.Lead=========");
-      
-      //中間テーブル登録
-      $prepIns001 = $dbh->prepare('INSERT INTO sfdcmiddle.middle_lead(id,sfid, schema,firstname,lastname,email) VALUES(:id,:sfid,:schema,:firstname,:lastname,:email)');
-      $prepIns001->execute(array($id,$sfid,$schema,$firstname,$lastname,$email));
-  }
-  
 
 }catch(PDOException $e){
   print("接続失敗");
