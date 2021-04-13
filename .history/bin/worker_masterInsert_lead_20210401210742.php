35031db8db61a39e7db82736fa60a185185bd0fa
you tou <d.yang@triplevantage.com>
2021-04-13 16:45:08 +0900

    added8

--- .history/bin/worker_masterInsert_lead_20210401210742.php
+++ .history/bin/worker_masterInsert_lead_20210401210742.php
@@ -11,29 +11,29 @@ $DBPASS = $dbopts["pass"];
 try{
   //DB接続
   $dbh = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");
-
+  
   //マスタ取り込み処理
    //SQL作成
-
-
+   
+   
   $sql = 'select a.sfid as asfid,a.schema as aschema,b.sfid as bsfid,b.schema as bschema,b.lastname as blastname,b.firstname as bfirstname,b.email as bemail from sfdcmiddle.middle_lead a ,sfdcmiddle.middle_lead b
                where a.lastname = b.lastname
                    and a.firstname = b.firstname
                    and a.email = b.email
                   and  a.schema != b.schema';
-
+  
   $stmt  = $dbh->query($sql);
 
   $keynew = ['','',''];
   $keyold = ['','',''];
-
+  
   //SQL実行
   foreach ($stmt as $row) {
       //指定Columnを一覧表示
 
-    //  print($row['asfid'].'\n');
+    //  print($row['asfid'].'\n');      
     //  print($row['aschema'].'\n');
-    //  print($row['bsfid'].'\n');
+    //  print($row['bsfid'].'\n');      
     //  print($row['bschema'].'\n');
     //  print($row['bemail'].'\n');
     //  print($row['blastname'].'\n');
@@ -45,7 +45,7 @@ try{
      $lastname = $row['blastname'];
      $firstname = $row['bfirstname'];
      $email = $row['bemail'];
-
+     
 
      if ($row === reset($stmt)){
          $keynew = [$firstname,$lastname,$email];
@@ -66,21 +66,21 @@ try{
             $firstname=$keynew[0];
             $lastname=$keynew[1];
             $email=$keynew[2];
-           $prepIns = $dbh->prepare('INSERT INTO sfdcmaster.master_lead(firstname,lastname,email,company,vctr__vectorno__c) VALUES(:firstname,:lastname,:email,:company,:vectorno__c)');
+           $prepIns = $dbh->prepare('INSERT INTO sfdcmaster.master_lead(firstname,lastname,email,company,vectorno__c) VALUES(:firstname,:lastname,:email,:company,:vectorno__c)');
            $prepIns->execute(array($firstname,$lastname,$email,$company,$vectorno__c));
-
+           
            //中間テーブルに統合IDを反映
-          $sqlUpdate = 'update sfdcmiddle.middle_lead set company = :company,vctr__vectorno__c=:vectorno__c WHERE firstname = :firstname and lastname=:lastname and email=:email';
+          $sqlUpdate = 'update sfdcmiddle.middle_lead set company = :company,vectorno__c=:vectorno__c WHERE firstname = :firstname and lastname=:lastname and email=:email';
           $stmtUpdate = $dbh->prepare($sqlUpdate);
-
+          
           $stmtUpdate->bindParam(':company', $company);
           $stmtUpdate->bindParam(':vectorno__c', $vectorno__c);
           $stmtUpdate->bindParam(':firstname', $firstname);
           $stmtUpdate->bindParam(':lastname', $lastname);
           $stmtUpdate->bindParam(':email', $email);
-
+          
           $stmtUpdate->execute();
-
+           
          }else{
              //keyoldのデータ登録
              print('=======keyoldのデータ登録=====');
@@ -90,28 +90,28 @@ try{
             $firstname=$keynew[0];
             $lastname=$keynew[1];
             $email=$keynew[2];
-           $prepIns = $dbh->prepare('INSERT INTO sfdcmaster.master_lead(firstname,lastname,email,company,vctr__vectorno__c) VALUES(:firstname,:lastname,:email,:company,:vectorno__c)');
+           $prepIns = $dbh->prepare('INSERT INTO sfdcmaster.master_lead(firstname,lastname,email,company,vectorno__c) VALUES(:firstname,:lastname,:email,:company,:vectorno__c)');
            $prepIns->execute(array($firstname,$lastname,$email,$company,$vectorno__c));
-
+           
            //中間テーブルに統合IDを反映
-          $sqlUpdate = 'update sfdcmiddle.middle_lead set company = :company,vctr__vectorno__c=:vectorno__c WHERE firstname = :firstname and lastname=:lastname and email=:email';
+          $sqlUpdate = 'update sfdcmiddle.middle_lead set company = :company,vectorno__c=:vectorno__c WHERE firstname = :firstname and lastname=:lastname and email=:email';
           $stmtUpdate = $dbh->prepare($sqlUpdate);
-
+          
          // $stmtUpdate->bindParam(':company', $company);
         //  $stmtUpdate->bindParam(':vectorno__c', $vectorno__c);
         //  $stmtUpdate->bindParam(':firstname', $firstname);
         //  $stmtUpdate->bindParam(':lastname', $lastname);
         //  $stmtUpdate->bindParam(':email', $email);
-
+          
           $stmtUpdate->execute(array($company,$vectorno__c,$firstname,$lastname,$email));
-
-
+           
+           
              //keyoldにkeynewを設定
              $keyold = $keynew;
        }
      }
-
-
+      
+      
   }
 
 
--- .history/bin/worker_outdataSelect_lead_20210401231941.php
+++ .history/bin/worker_outdataSelect_lead_20210401231941.php
@@ -11,18 +11,18 @@ $DBPASS = $dbopts["pass"];
 try{
   //DB接続
   $dbh = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");
-
+  
    //出力中間データ取り込み処理
    //SQL作成
-
-    $sql = 'select middle.id as id,middle.sfid as sfid,middle.schema as schema,middle.firstname as firstname ,middle.lastname as lastname,middle.email as email,master.company as company,middle.vctr__vectorno__c as vctr__vectorno__c from sfdcmiddle.middle_lead middle ,sfdcmaster.master_lead master
-              where middle.vctr__vectorno__c = master.vctr__vectorno__c
+   
+    $sql = 'select middle.id as id,middle.sfid as sfid,middle.schema as schema,middle.firstname as firstname ,middle.lastname as lastname,middle.email as email,master.company as company,middle.vectorno__c as vectorno__c from sfdcmiddle.middle_lead middle ,sfdcmaster.master_lead master
+              where middle.vectorno__c = master.vectorno__c
                 and middle.firstname = master.firstname
                 and middle.lastname = master.lastname
                 and middle.email = master.email';
-
+  
     $stmt  = $dbh->query($sql);
-
+  
   //SQL実行
   foreach ($stmt as $row) {
       //指定Columnを一覧表示
@@ -33,22 +33,22 @@ try{
       print($row['lastname'].' ');
       print($row['email'].' ');
       print($row['company'].' ');
-      print($row['vctr__vectorno__c'].' ');
-
-
-     $id=$row['id'];
+      print($row['vectorno__c'].' ');
+    
+    
+     $id=$row['id'];    
      $sfid=$row['sfid'];
      $schema=$row['schema'];
      $firstname = $row['firstname'];
      $lastname = $row['lastname'];
      $email = $row['email'];
      $company = $row['company'];
-     $vectorno__c = $row['vctr__vectorno__c'];
-
+     $vectorno__c = $row['vectorno__c'];
+     
      //出力中間テーブル登録
-     $prepIns = $dbh->prepare('INSERT INTO sfdcmiddle.middle_out_lead(id,sfid,schema,firstname,lastname,email,company,vctr__vectorno__c) VALUES(:id,:sfid,:schema,:firstname,:lastname,:email,:company,:vectorno__c)');
+     $prepIns = $dbh->prepare('INSERT INTO sfdcmiddle.middle_out_lead(id,sfid,schema,firstname,lastname,email,company,vectorno__c) VALUES(:id,:sfid,:schema,:firstname,:lastname,:email,:company,:vectorno__c)');
      $prepIns->execute(array($id,$sfid,$schema,$firstname,$lastname,$email,$company,$vectorno__c));
-
+   
   }
 
 }catch(PDOException $e){
